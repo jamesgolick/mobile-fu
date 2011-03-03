988d460b2faa5039de0fa5bb2b2b1b39dcc6922e
James Golick <jamesgolick@gmail.com>
2011-03-03 11:36:49 -0800

    we don't need that anymore

--- lib/mobile_fu.rb
+++ lib/mobile_fu.rb
@@ -2,15 +2,6 @@ module ActionController
   module MobileFu
     class DeviceInfo < Struct.new(:operating_system, :version); end
 
-    # These are various strings that can be found in mobile devices.  Please feel free
-    # to add on to this list.
-    MOBILE_USER_AGENTS =  'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
-                          'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
-                          'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
-                          'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
-                          'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|' +
-                          'mobile'
-    
     def self.included(base)
       base.extend(ClassMethods)
     end
